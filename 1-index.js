d973d958ca8b878f67935d530657eae612f475af
Alina <107263631+AlinaNarolska@users.noreply.github.com>
2022-10-23 19:22:12 +0300

    task 3-5

--- 1-index.js
+++ 1-index.js
@@ -470,12 +470,88 @@
 ? Відфільтруйте масив heroes за ключем об'єкта franchise.
 */
 
-const heroes = [
-  { name: "Batman", franchise: "DC" },
-  { name: "Ironman", franchise: "Marvel" },
-  { name: "Thor", franchise: "Marvel" },
-  { name: "Superman", franchise: "DC" },
-];
+// const heroes = [
+//   { name: "Batman", franchise: "DC" },
+//   { name: "Ironman", franchise: "Marvel" },
+//   { name: "Thor", franchise: "Marvel" },
+//   { name: "Superman", franchise: "DC" },
+// ];
 
-const franchiseArray = heroes.filter(({ franchise }) => franchise === "Marvel");
-console.log(franchiseArray);
+// const franchiseArray = heroes.filter(({ franchise }) => franchise === "Marvel");
+// console.log(franchiseArray);
+// 3-5
+// ? Нехай функція filterByPrice повертає масив автомобілів ціна яких
+// ? менше ніж значення параметра threshold.
+// */
+// const cars = [
+//   {
+//     make: "Honda",
+//     model: "CR-V",
+//     type: "suv",
+//     amount: 14,
+//     price: 24045,
+//     onSale: true,
+//   },
+//   {
+//     make: "Honda",
+//     model: "Accord",
+//     type: "sedan",
+//     amount: 2,
+//     price: 22455,
+//     onSale: true,
+//   },
+//   {
+//     make: "Mazda",
+//     model: "Mazda 6",
+//     type: "sedan",
+//     amount: 8,
+//     price: 24195,
+//     onSale: false,
+//   },
+//   {
+//     make: "Mazda",
+//     model: "CX-9",
+//     type: "suv",
+//     amount: 7,
+//     price: 31520,
+//     onSale: true,
+//   },
+//   {
+//     make: "Toyota",
+//     model: "4Runner",
+//     type: "suv",
+//     amount: 19,
+//     price: 34210,
+//     onSale: false,
+//   },
+
+//   {
+//     make: "Ford",
+//     model: "F-150",
+//     type: "truck",
+//     amount: 11,
+//     price: 27110,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "Fusion",
+//     type: "sedan",
+//     amount: 13,
+//     price: 22120,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "Explorer",
+//     type: "suv",
+//     amount: 6,
+//     price: 31660,
+//     onSale: false,
+//   },
+// ];
+// const filterByPrice = (array, threshold) => {
+//   return array.filter((item) => item.price < threshold);
+// };
+// console.log(filterByPrice(cars, 25000));
+// 3-6

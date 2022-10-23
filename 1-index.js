ce7e2b33288a95adbbd850019247de824401f968
Bogdan <bogdanberezka@gmail.com>
2022-10-23 20:56:25 +0300

    task_3-12

--- 1-index.js
+++ 1-index.js
@@ -483,73 +483,73 @@
 // ? Нехай функція filterByPrice повертає масив автомобілів ціна яких
 // ? менше ніж значення параметра threshold.
 // */
-const cars = [
-  {
-    make: "Honda",
-    model: "CR-V",
-    type: "suv",
-    amount: 14,
-    price: 24045,
-    onSale: true,
-  },
-  {
-    make: "Honda",
-    model: "Accord",
-    type: "sedan",
-    amount: 2,
-    price: 22455,
-    onSale: true,
-  },
-  {
-    make: "Mazda",
-    model: "Mazda 6",
-    type: "sedan",
-    amount: 8,
-    price: 24195,
-    onSale: false,
-  },
-  {
-    make: "Mazda",
-    model: "CX-9",
-    type: "suv",
-    amount: 7,
-    price: 31520,
-    onSale: true,
-  },
-  {
-    make: "Toyota",
-    model: "4Runner",
-    type: "suv",
-    amount: 19,
-    price: 34210,
-    onSale: false,
-  },
+// const cars = [
+//   {
+//     make: "Honda",
+//     model: "CR-V",
+//     type: "suv",
+//     amount: 14,
+//     price: 24045,
+//     onSale: true,
+//   },
+//   {
+//     make: "Honda",
+//     model: "Accord",
+//     type: "sedan",
+//     amount: 2,
+//     price: 22455,
+//     onSale: true,
+//   },
+//   {
+//     make: "Mazda",
+//     model: "Mazda 6",
+//     type: "sedan",
+//     amount: 8,
+//     price: 24195,
+//     onSale: false,
+//   },
+//   {
+//     make: "Mazda",
+//     model: "CX-9",
+//     type: "suv",
+//     amount: 7,
+//     price: 31520,
+//     onSale: true,
+//   },
+//   {
+//     make: "Toyota",
+//     model: "4Runner",
+//     type: "suv",
+//     amount: 19,
+//     price: 34210,
+//     onSale: false,
+//   },
 
-  {
-    make: "Ford",
-    model: "F-150",
-    type: "truck",
-    amount: 11,
-    price: 27110,
-    onSale: true,
-  },
-  {
-    make: "Ford",
-    model: "Fusion",
-    type: "sedan",
-    amount: 13,
-    price: 22120,
-    onSale: true,
-  },
-  {
-    make: "Ford",
-    model: "Explorer",
-    type: "suv",
-    amount: 6,
-    price: 31660,
-    onSale: false,
-  },
-];
+//   {
+//     make: "Ford",
+//     model: "F-150",
+//     type: "truck",
+//     amount: 11,
+//     price: 27110,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "Fusion",
+//     type: "sedan",
+//     amount: 13,
+//     price: 22120,
+//     onSale: true,
+//   },
+//   {
+//     make: "Ford",
+//     model: "Explorer",
+//     type: "suv",
+//     amount: 6,
+//     price: 31660,
+//     onSale: false,
+//   },
+// ];
 // const filterByPrice = (array, threshold) => {
 //   return array.filter((item) => item.price < threshold);
 // };
@@ -612,20 +612,41 @@ const cars = [
 // }
 // console.log(positiveValues);
 
-const colorPickerOptions = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F51B5" },
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
+
+// console.log(ownFind(colorPickerOptions, (option) => option.label === "blue"));
+
+// function ownFind(array, callback) {
+//   for (let i = 0; i <= array.length; i += 1) {
+//     if (callback(array[i], i, array)) {
+//       return array[i];
+//     }
+//   }
+// }
+
+const students = [
+  { name: "Манго", score: 80 },
+  { name: "Поли", score: 50 },
+  { name: "Аякс", score: 30 },
+  { name: "Киви", score: 90 },
+  { name: "Хьюстон", score: 60 },
 ];
 
-console.log(ownFind(colorPickerOptions, (option) => option.label === "blue"));
-
-function ownFind(array, callback) {
-  for (let i = 0; i <= array.length; i += 1) {
-    if (callback(array[i], i, array)) {
-      return array[i];
-    }
+function ownReduce(array, callback, initialValue) {
+  for (let i = 0; i < array.length; i += 1) {
+    initialValue = callback(initialValue, array[i], i, array);
   }
+  return initialValue;
 }
+
+// const totalScore = ownReduce(students, (acc, item) => acc + item.score, 0);
+
+const totalScore = ownReduce(students, (acc, item) => [...acc, item.name], []);
+
+console.log(totalScore);
